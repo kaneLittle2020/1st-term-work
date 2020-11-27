65f9729bdbc476a21d68b551a81ded3b1fbb2b1e
kaneLittle2020 <5littk17@solent.ac.uk>
2020-11-27 20:04:31 +0000

    stuuf

--- main.py
+++ main.py
@@ -1,43 +1,8 @@
-print ("where should i look, Bathroom, Bedroom, Lab?") 
-place = input () 
-
-if (place == 'bedroom'):
-  print ("you are now in the bedroom, where in the bedroom do you want to look?")
-  bedroomloc = input ()
-  if (bedroomloc == 'under the bed') :
-    print ("Nothing seems to be here")
-  else: 
-    print ("Found some mess but, no batteries")
-
-elif  (place == 'bathroom'):
-  print("Where in the bathroom should I look?")
-  bathloc = input ()
-  if (bathloc == 'bathtub'):
-    print ("Found a rubber duck but no battery")
-  else:
-    print("Found a wet surface but no battery.")
-
-elif (place == "lab"):
-    print("Where in the lab should I look?")
-    labplace = input()
-
-    if (labplace == "on the table"):
-        print("Yes! I found my battery!")
-    else:
-        print("Found some tools but no battery.")
-
-# Handle unknown place
+print ("What type of adventure should i have?")
+adventuret = input ()
+if ( ( adventuret == "Scary") or (adventuret == "short")):
+  print ("Entering into a dark forest")
+elif ((adventuret == 'long') or(adventuret == 'safe')):
+  print ("you a pussy ass bitch")
 else:
-    print("I am not sure where that place is located.")
-
-
-
-
-
-
-
-
-
-
-
-
+  print ("Not sure what adventure you want")
